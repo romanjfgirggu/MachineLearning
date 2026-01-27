ac09a30c46e2213ec9df9012356ac86441b7fcef
aleks382 <aleksandrkurambaev@gmail.com>
2026-01-27 14:41:20 +0300

    Начат 4 модуль

--- Проект №6 (последний срез)/Application (Module_D)/main.py
+++ Проект №6 (последний срез)/Application (Module_D)/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/dangerous/{lat}/{long}")
 def get_dangerous(lat: float, lon: float):
     
+
+    return 0
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
